e8491090589a3c2d77d8fe8376d1fc9602675b2e
SthephanySouza <101808324+SthephanySouza@users.noreply.github.com>
2022-10-03 08:26:08 -0300

    Update
    Alteração dos inserts venda e nota fiscal

--- Distribuidora/Aaaaaaaaaa.sql
+++ Distribuidora/Aaaaaaaaaa.sql
@@ -98,7 +98,7 @@ create table tbItemCompra (
 create table tbVenda (
  IdCliente int not null,
  foreign key (IdCliente) references tbCliente(Id),
- NumeroVenda int primary key,
+ NumeroVenda int primary key auto_increment,
  DataVenda datetime not null default(current_timestamp()),
  TotalVenda decimal(7, 2) not null,
  NotaFiscal int,
@@ -125,15 +125,6 @@ begin
 	end if;
 end $$
 
-call spInsertForn('Revenda Chico Loco', 1245678937123, 11934567897);
-call spInsertForn('José Faz Tudo S/A', 1345678937123, 11934567898);
-call spInsertForn('Vadalto Entregas', 1445678937123, 11934567899);
-call spInsertForn('Astrogildo das Estrela', 1545678937123, 11934567800);
-call spInsertForn('Amoroso e Doce', 1645678937123, 11934567801);
-call spInsertForn('Marcelo Dedal', 1745678937123, 11934567802);
-call spInsertForn('Franciscano Cachaça', 1845678937123, 11934567803);
-call spInsertForn('Joãozinho Chupeta', 1945678937123, 11934567804);
-
 delimiter $$
 create procedure spInsertCidade(vCidade varchar(200))
 begin
@@ -141,15 +132,6 @@ if not exists (select IdCidade from tbCidade where Cidade = vCidade) then
 	insert into tbCidade(Cidade) values (vCidade);
 end if;
 end $$
-
-call spInsertCidade('Rio de Janeiro');
-call spInsertCidade('São Carlos');
-call spInsertCidade('Campinas');
-call spInsertCidade('Franco da Rocha');
-call spInsertCidade('Osasco');
-call spInsertCidade('Pirituba');
-call spInsertCidade('Lapa');
-call spInsertCidade('Ponta Grossa');
 
 delimiter $$
 create procedure spInsertUF(vUF char(2))
@@ -159,10 +141,6 @@ if not exists (select IdUf from tbUF where UF = vUF) then
 end if;
 end $$
 
-call spInsertUF('SP');
-call spInsertUF('RJ');
-call spInsertUF('RS');
-
 delimiter $$
 create procedure spInsertBairro(vBairro varchar(200))
 begin
@@ -170,11 +148,6 @@ if not exists (select IdBairro from tbBairro where Bairro = vBairro) then
 	insert into tbBairro(Bairro) values (vBairro);
 end if;
 end $$
-
-call spInsertBairro('Aclimação');
-call spInsertBairro('Capão Redondo');
-call spInsertBairro('Pirituba');
-call spInsertBairro('Liberdade');
 
 delimiter $$
 create procedure spInsertProduto(vCodBarras decimal(14,0), vNome varchar(200), vValorUnitario decimal(6, 2), vQtd int)
@@ -185,19 +158,6 @@ begin
 		select 'Já Existe';
     end if;
 end $$
-
-call spInsertProduto('12345678910111', 'Rei de Papel Mache', '54.61', '120');
-call spInsertProduto('12345678910112', 'Bolinha de Sabão', '100.45', '120');
-call spInsertProduto('12345678910113', 'Carro Bate Bate', '44.00', '120');
-call spInsertProduto('12345678910114', 'Bola Furada', '10.00', '120');
-call spInsertProduto('12345678910115', 'Maçã Laranja', '99.44', '120');
-call spInsertProduto('12345678910116', 'Boneco do Hitler', '124.00', '200');
-call spInsertProduto('12345678910117', 'Farinha de Suruí', '50.00', '200');
-call spInsertProduto('12345678910118', 'Zelador de Cemitério', '24.50', '100');
-
-call spInsertProduto(12345678910130, 'Camiseta de Poliéster', 35.61, 100);
-call spInsertProduto(12345678910131, 'Blusa Frio Moletom', 200.00, 100);
-call spInsertProduto(12345678910132, 'Vestido Decote Redondo', 144.00, 50);
 
 delimiter $$
 create procedure spInsertEndereco(vCEP decimal(8,0), vLogradouro varchar(200), vBairro varchar(200), vCidade varchar(200), vUF char(2))
@@ -223,15 +183,6 @@ if not exists (select CEP from tbEndereco where CEP = vCEP) then
 	(vCEP, vLogradouro, @IdBairro, @IdCidade, @IdUF); 
 end if;
 end $$
-
-call spInsertEndereco(12345050, 'Rua da Federal', 'Lapa', 'São Paulo', 'SP');
-call spInsertEndereco(12345051, 'Av Brasil', 'Lapa', 'Campinas', 'SP');
-call spInsertEndereco(12345052, 'Rua Liberdade', 'Consolação', 'São Paulo', 'SP');
-call spInsertEndereco(12345053, 'Av Paulista', 'Penha', 'Rio de Janeiro', 'RJ');
-call spInsertEndereco(12345054, 'Rua Ximbú', 'Penha', 'Rio de Janeiro', 'RJ');
-call spInsertEndereco(12345055, 'Rua Piu XI', 'Penha', 'Campina', 'SP');
-call spInsertEndereco(12345056, 'Rua Chocolate', 'Aclimação', 'Barra Mansa', 'RJ');
-call spInsertEndereco(12345057, 'Rua Pão na Chapa', 'Barra Funda', 'Ponto Grossa', 'RS');
 
 delimiter $$
 create procedure spInsertCliente (vNome varchar(50), vNumEnd decimal(6,0), vCompEnd varchar(50), vCEP decimal(8,0), vCPF decimal(11,0), vRG decimal(8,0), vRgDig char(1), vNasc date,
@@ -265,12 +216,6 @@ begin
 		select "Existe";
 	end if;
 end $$
-
-call spInsertCliente('Pimpão', 325, null, 12345051, 12345678911, 12345678, 0, '2000-12-10', 'Av. Brasil', 'Lapa', 'Campinas', 'SP');
-call spInsertCliente('Disney Chaplin', 89, 'Ap. 12', 12345053, 12345678912, 12345679, 0, '2001-11-21', 'Av. Paulista', 'Penha', 'Rio de Janeiro', 'RJ');
-call spInsertCliente('Marciano', 744, null, 12345054, 12345678913, 12345680, 0, '2001-06-01', 'Rua Ximbú', 'Penha', 'Rio de Janeiro', 'RJ');
-call spInsertCliente('Lança Perfume', 128, null, 12345059, 12345678914, 12345681, 'X', '2004-04-05', 'Rua Veia', 'Jardim Santa Isabel', 'Cuiabá', 'MT');
-call spInsertCliente('Remédio Amargo', 2485, null, 12345058, 12345678915, 12345682, 0, '2002-07-15', 'Av. Nova', 'Jardim Santa Isabel', 'Cuiabá', 'MT');
 
 delimiter $$
 create procedure spInsertCliPJ (vNome varchar(50), vCNPJ decimal(14,0), vIE decimal(11,0), vCEP decimal(8,0), vLogradouro varchar(200), vNumEnd decimal(6,0), vCompEnd varchar(50),
@@ -306,12 +251,6 @@ begin
 	end if;
 end $$
 
-call spInsertCliPJ('Paganada', 12345678912345, 98765432198, 12345051, 'Av. Brasil', 159, null, 'Lapa', 'Campinas', 'SP');
-call spInsertCliPJ('Caloteando', 12345678912346, 98765432199, 12345053, 'Av. Paulista', 69, null, 'Penha', 'Rio de Janeiro', 'RJ');
-call spInsertCliPJ('Semgrana', 12345678912347, 98765432100, 12345060, 'Rua dos Amores', 189, null, 'Sei Lá', 'Recife', 'PE');
-call spInsertCliPJ('Cemreais', 12345678912348, 98765432101, 12345060, 'Rua dos Amores', 5024, 'Sala 23', 'Sei Lá', 'Recife', 'PE');
-call spInsertCliPJ('Durango', 12345678912349, 98765432102, 12345060, 'Rua dos Amores', 1254, null, 'Sei Lá', 'Recife', 'PE');
-
 delimiter $$
 create procedure spInsertCompra(vNotaFiscal int, vFornecedor varchar(200), vDataCompra date, vCodBarras decimal(14,0), vValorItem decimal(6,2),
 vQtd int, vQtdTotal int, vValorTotal decimal(8,2))
@@ -323,31 +262,21 @@ begin
         insert into tbItemCompra(Qtd, ValorItem, NotaFiscal, CodBarras) values (vQtd, vValorItem, vNotaFiscal, vCodBarras);
 end $$
 
-call spInsertCompra(8459, 'Amoroso e Doce', '2018-05-01', 12345678910111, 22.22, 200, 700, 21944.00);
-call spInsertCompra(2482, 'Revenda Chico Loco', '2020-04-22', 12345678910112, 40.50, 180, 180, 7290.00);
-call spInsertCompra(21563, 'Marcelo Dedal', '2020-07-12', 12345678910113, 3.00, 300, 300, 900.00);
-call spInsertCompra(8459, 'Amoroso e Doce', '2020-12-04', 12345678910114, 35.00, 500, 700, 21944.00);
-call spInsertCompra(156354, 'Revenda Chico Loco', '2021-11-23', 12345678910115, 54.00, 350, 350, 18900.00);
-
-drop procedure spInsertNF;
-describe tbVenda;
-
 delimiter $$
-create procedure spInsertVenda(vNumVenda int, vCliente varchar(200), vDataVenda char(10), vCodBarras decimal(14,0), vValorItem decimal(6,2), vQtd int, vTotalVenda int, vNF int)
+create procedure spInsertVenda(vCliente varchar(200), vCodBarras decimal(14,0), vQtd int, vNF int)
 begin
-	set @IdCli = (select Id from tbCliente where Nome = vCliente);
-    set @DataVenda = str_to_date(vDataVenda, "%d/%m/%Y");
+	declare vValorItem decimal(6,2);
+    declare vTotalVenda int;
+    
+    set @IdCli = (select Id from tbCliente where Nome = vCliente);
+    set @DataVenda = current_timestamp();
     set @CodBarras = (select CodBarras from tbProduto where CodBarras = vCodBarras);
+	set vValorItem = (select ValorUnitario from tbProduto where CodBarras = vCodBarras);
+	set vTotalVenda = vValorItem * vQtd;
 
-	if not exists (select NumeroVenda from tbVenda where NumeroVenda = vNumVenda) then
-		insert into tbVenda(IdCliente, NumeroVenda, DataVenda, TotalVenda, NotaFiscal) values (@IdCli, vNumVenda, @DataVenda, vTotalVenda, vNF);
-        insert into tbItemVenda(NumeroVenda, CodBarras, Qtd, ValorItem) values (vNumVenda, @CodBarras, vQtd, vValorItem);
-	end if;
+	insert into tbVenda(IdCliente, DataVenda, TotalVenda, NotaFiscal) values (@IdCli, @DataVenda, vTotalVenda, vNF);
+	insert into tbItemVenda(NumeroVenda, CodBarras, Qtd, ValorItem) values ((select NumeroVenda from tbVenda order by NumeroVenda desc limit 1), @CodBarras, vQtd, vValorItem);
 end $$
-
-call spInsertVenda(1, 'Pimpão', '22/08/2022', 12345678910111, 54.61, 1, 54.61, null);
-call spInsertVenda(2, 'Lança Perfume', '22/08/2022', 12345678910112, 100.45, 2, 200.90, null);
-call spInsertVenda(3, 'Pimpão', '22/08/2022', 12345678910113, 44.00, 1, 44.00, null);
 
 delimiter $$
 create procedure spInsertNF(vNF int, vCliente varchar(200), vDataEmissao char(10))
@@ -359,14 +288,7 @@ begin
 	if not exists (select NF from tbNotaFiscal where NF = vNF) then
 		insert into tbNotaFiscal(NF, TotalNota, DataEmissao) values (vNF, @ValorTotal, @DataEmissao);
 	end if;
-
-   	if not exists (select NotaFiscal from tbVenda where NotaFiscal = vNF) then
-		update tbVenda set NotaFiscal = vNF where IdCliente = @IdCli;
-	end if;
 end $$
-
-call spInsertNF(359, 'Pimpão', '29/08/2022');
-call spInsertNF(360, 'Lança Perfume', '29/08/2022');
 
 delimiter $$
 create procedure spDeleteProd(vCodBarras decimal(14,0))
@@ -377,9 +299,6 @@ create procedure spDeleteProd(vCodBarras decimal(14,0))
     end;
 $$
 
-call spDeleteProd(12345678910116);
-call spDeleteProd(12345678910117);
-
 delimiter $$
 create procedure spUpdateProd(vCodBarras decimal(14,0), vNome varchar(200), vValorUnitario decimal(6, 2))
 	begin
@@ -388,10 +307,6 @@ create procedure spUpdateProd(vCodBarras decimal(14,0), vNome varchar(200), vVal
 		end if;
     end;
 $$
-
-call spUpdateProd(12345678910111, 'Rei de Papel Mache', 64.50);
-call spUpdateProd(12345678910112, 'Bolinha de Sabão', 120.00);
-call spUpdateProd(12345678910113, 'Carro Bate Bate', 64.00);
 
 delimiter $$ 
 create procedure spSelectProd()
@@ -435,8 +350,6 @@ delimiter $$
 	end;
 $$
 
-call spInsertProduto(12345678910119, 'Água mineral', 1.99, 500);
-
 delimiter $$
 	create trigger tgrHistoricoUpdate after update on tbProduto
 		for each row
@@ -458,11 +371,6 @@ delimiter $$
 	end;
 $$
 
-call spUpdateProd(12345678910119, 'Água mineral', 2.99);
-
-call spInsertVenda(4, 'Disney Chaplin', '26/09/2022', 12345678910111, 64.50, 1, 64.50, null);
-call spSelectCli("Disney Chaplin");
-
 delimiter $$
 create trigger trgCtrlEstoque after insert on tbItemVenda for each row
 	begin
@@ -473,8 +381,6 @@ create trigger trgCtrlEstoque after insert on tbItemVenda for each row
         update tbProduto set Qtd = Qtd - vVendido where CodBarras = vProd;
 	end;
 $$
-
-call spInsertVenda(5, 'Paganada', '26/09/2022', 12345678910114, 10, 15, 150, null);
 
 delimiter $$
 create trigger trgCtrlEstoqueCompra after insert on tbItemCompra for each row
@@ -487,9 +393,100 @@ create trigger trgCtrlEstoqueCompra after insert on tbItemCompra for each row
 	end;
 $$
 
-call spInsertCompra(10548, 'Amoroso e Doce', '2022-09-10', 12345678910111, 40.00, 100, 100, 4000.00);
+-- chamando os atalhos
+call spInsertForn('Revenda Chico Loco', 1245678937123, 11934567897);
+call spInsertForn('José Faz Tudo S/A', 1345678937123, 11934567898);
+call spInsertForn('Vadalto Entregas', 1445678937123, 11934567899);
+call spInsertForn('Astrogildo das Estrela', 1545678937123, 11934567800);
+call spInsertForn('Amoroso e Doce', 1645678937123, 11934567801);
+call spInsertForn('Marcelo Dedal', 1745678937123, 11934567802);
+call spInsertForn('Franciscano Cachaça', 1845678937123, 11934567803);
+call spInsertForn('Joãozinho Chupeta', 1945678937123, 11934567804);
+
+call spInsertCidade('Rio de Janeiro');
+call spInsertCidade('São Carlos');
+call spInsertCidade('Campinas');
+call spInsertCidade('Franco da Rocha');
+call spInsertCidade('Osasco');
+call spInsertCidade('Pirituba');
+call spInsertCidade('Lapa');
+call spInsertCidade('Ponta Grossa');
+
+call spInsertUF('SP');
+call spInsertUF('RJ');
+call spInsertUF('RS');
+
+call spInsertBairro('Aclimação');
+call spInsertBairro('Capão Redondo');
+call spInsertBairro('Pirituba');
+call spInsertBairro('Liberdade');
+
+call spInsertProduto('12345678910111', 'Rei de Papel Mache', '54.61', '120');
+call spInsertProduto('12345678910112', 'Bolinha de Sabão', '100.45', '120');
+call spInsertProduto('12345678910113', 'Carro Bate Bate', '44.00', '120');
+call spInsertProduto('12345678910114', 'Bola Furada', '10.00', '120');
+call spInsertProduto('12345678910115', 'Maçã Laranja', '99.44', '120');
+call spInsertProduto('12345678910116', 'Boneco do Hitler', '124.00', '200');
+call spInsertProduto('12345678910117', 'Farinha de Suruí', '50.00', '200');
+call spInsertProduto('12345678910118', 'Zelador de Cemitério', '24.50', '100');
+
+call spInsertEndereco(12345050, 'Rua da Federal', 'Lapa', 'São Paulo', 'SP');
+call spInsertEndereco(12345051, 'Av Brasil', 'Lapa', 'Campinas', 'SP');
+call spInsertEndereco(12345052, 'Rua Liberdade', 'Consolação', 'São Paulo', 'SP');
+call spInsertEndereco(12345053, 'Av Paulista', 'Penha', 'Rio de Janeiro', 'RJ');
+call spInsertEndereco(12345054, 'Rua Ximbú', 'Penha', 'Rio de Janeiro', 'RJ');
+call spInsertEndereco(12345055, 'Rua Piu XI', 'Penha', 'Campina', 'SP');
+call spInsertEndereco(12345056, 'Rua Chocolate', 'Aclimação', 'Barra Mansa', 'RJ');
+call spInsertEndereco(12345057, 'Rua Pão na Chapa', 'Barra Funda', 'Ponto Grossa', 'RS');
+
+call spInsertCliente('Pimpão', 325, null, 12345051, 12345678911, 12345678, 0, '2000-12-10', 'Av. Brasil', 'Lapa', 'Campinas', 'SP');
+call spInsertCliente('Disney Chaplin', 89, 'Ap. 12', 12345053, 12345678912, 12345679, 0, '2001-11-21', 'Av. Paulista', 'Penha', 'Rio de Janeiro', 'RJ');
+call spInsertCliente('Marciano', 744, null, 12345054, 12345678913, 12345680, 0, '2001-06-01', 'Rua Ximbú', 'Penha', 'Rio de Janeiro', 'RJ');
+call spInsertCliente('Lança Perfume', 128, null, 12345059, 12345678914, 12345681, 'X', '2004-04-05', 'Rua Veia', 'Jardim Santa Isabel', 'Cuiabá', 'MT');
+call spInsertCliente('Remédio Amargo', 2485, null, 12345058, 12345678915, 12345682, 0, '2002-07-15', 'Av. Nova', 'Jardim Santa Isabel', 'Cuiabá', 'MT');
+
+call spInsertCliPJ('Paganada', 12345678912345, 98765432198, 12345051, 'Av. Brasil', 159, null, 'Lapa', 'Campinas', 'SP');
+call spInsertCliPJ('Caloteando', 12345678912346, 98765432199, 12345053, 'Av. Paulista', 69, null, 'Penha', 'Rio de Janeiro', 'RJ');
+call spInsertCliPJ('Semgrana', 12345678912347, 98765432100, 12345060, 'Rua dos Amores', 189, null, 'Sei Lá', 'Recife', 'PE');
+call spInsertCliPJ('Cemreais', 12345678912348, 98765432101, 12345060, 'Rua dos Amores', 5024, 'Sala 23', 'Sei Lá', 'Recife', 'PE');
+call spInsertCliPJ('Durango', 12345678912349, 98765432102, 12345060, 'Rua dos Amores', 1254, null, 'Sei Lá', 'Recife', 'PE');
+
+call spInsertCompra(8459, 'Amoroso e Doce', '2018-05-01', 12345678910111, 22.22, 200, 700, 21944.00);
+call spInsertCompra(2482, 'Revenda Chico Loco', '2020-04-22', 12345678910112, 40.50, 180, 180, 7290.00);
+call spInsertCompra(21563, 'Marcelo Dedal', '2020-07-12', 12345678910113, 3.00, 300, 300, 900.00);
+call spInsertCompra(8459, 'Amoroso e Doce', '2020-12-04', 12345678910114, 35.00, 500, 700, 21944.00);
+call spInsertCompra(156354, 'Revenda Chico Loco', '2021-11-23', 12345678910115, 54.00, 350, 350, 18900.00);
+
+call spInsertVenda('Pimpão', 12345678910111, 1, null);
+call spInsertVenda('Lança Perfume', 12345678910112, 2, null);
+call spInsertVenda('Pimpão', 12345678910113, 1, null);
+
+call spInsertNF(359, 'Pimpão', '29/08/2022');
+call spInsertNF(360, 'Lança Perfume', '29/08/2022');
+
+call spInsertProduto(12345678910130, 'Camiseta de Poliéster', 35.61, 100);
+call spInsertProduto(12345678910131, 'Blusa Frio Moletom', 200.00, 100);
+call spInsertProduto(12345678910132, 'Vestido Decote Redondo', 144.00, 50);
+
+call spDeleteProd(12345678910116);
+call spDeleteProd(12345678910117);
+
+call spUpdateProd(12345678910111, 'Rei de Papel Mache', 64.50);
+call spUpdateProd(12345678910112, 'Bolinha de Sabão', 120.00);
+call spUpdateProd(12345678910113, 'Carro Bate Bate', 64.00);
+
+call spInsertProduto(12345678910119, 'Água mineral', 1.99, 500);
+call spUpdateProd(12345678910119, 'Água mineral', 2.99);
+
+call spInsertVenda('Disney Chaplin', 12345678910111, 1, null);
+
+call spSelectCli("Disney Chaplin");
+
+call spInsertVenda('Paganada', 12345678910114, 15, null);
 
 call spInsertProduto(12345678910199, 'Boneca', 21.00, 200);
+
+call spInsertCompra(10548, 'Amoroso e Doce', '2022-09-10', 12345678910111, 40.00, 100, 100, 4000.00);
 
 -- selecionando tabelas
 select * from tbCliente;
